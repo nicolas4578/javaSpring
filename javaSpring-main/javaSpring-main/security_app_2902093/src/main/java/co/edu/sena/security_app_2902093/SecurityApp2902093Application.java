df5d3e5f0264e34c53b01b31e665202a37072eb0
nicolas4578 <159819017+nicolas4578@users.noreply.github.com>
2024-05-08 16:44:52 -0500

    Add files via upload

---
+++ javaSpring-main/javaSpring-main/security_app_2902093/src/main/java/co/edu/sena/security_app_2902093/SecurityApp2902093Application.java
@@ -0,0 +1,13 @@
+package co.edu.sena.security_app_2902093;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApp2902093Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApp2902093Application.class, args);
+	}
+
+}
