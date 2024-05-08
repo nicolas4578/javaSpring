2e5f7e64468b351fa53080abd69f189d447cc4e1
nicolas4578 <159819017+nicolas4578@users.noreply.github.com>
2024-05-08 10:21:09 -0500

    Add files via upload

---
+++ security_app_2902093/src/main/java/co/edu/sena/security_app_2902093/controller/TestAuthController.java
@@ -0,0 +1,24 @@
+package co.edu.sena.security_app_2902093.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+
+
+public class TestAuthController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+
+    @GetMapping("/hello-secured")
+    public String helloSecured(){
+        return "Hello world Secured";
+    }
+
+}
